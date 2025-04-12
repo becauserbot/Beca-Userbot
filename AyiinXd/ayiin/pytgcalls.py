15a397ac0db6346a556007cab9d70a92af04c1fd
becaubot <renoirab4@gmail.com>
2025-04-12 09:19:08 +0700

    Update pytgcalls.py

--- AyiinXd/ayiin/pytgcalls.py
+++ AyiinXd/ayiin/pytgcalls.py
@@ -64,7 +64,7 @@ class Ayiin:
         try:
             await bot(
                 functions.phone.CreateGroupCallRequest(
-                    self._chat, title="🎧 Ayiin Music 🎧"
+                    self._chat, title="🎧 Beca Ngobrol 🎧"
                 )
             )
         except Exception as e:
